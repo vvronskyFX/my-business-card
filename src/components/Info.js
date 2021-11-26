cb8f66c846e3fb7fe57c815febb89def1c31195b
Vladlen Vronsky <64465297+vvronskyFX@users.noreply.github.com>
2021-11-26 18:34:32 -0500

    Changed some of my interests

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,7 +6,7 @@ export default function Info() {
             <h1 className="info--about">About</h1>
             <p className="info--para">I am a fullstack developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and I am always looking for new things to learn.</p>
             <h1 className="info--interests">Interests</h1>
-            <p className="info--para">Food expert. Pilot. Reader. Web3 fanatic. Clean food buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic. Ice baths rock!</p>
+            <p className="info--para">Trader. Pilot. Reader. Web3 fanatic. Clean food buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic. Ice baths rock!</p>
         </section>
     )
 }
